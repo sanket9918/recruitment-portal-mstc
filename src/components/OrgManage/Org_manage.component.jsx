37e5e9cc57339ae9bfa97bb346727e816fc26f7b
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2020-05-09 08:18:50 +0530

    fixes

--- src/components/OrgManage/Org_manage.component.jsx
+++ src/components/OrgManage/Org_manage.component.jsx
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
 import Navbar1 from '../navbar.component';
 import Footer from '../footer.component';
-import { Container, Col, Row, Button ,Collapse} from 'reactstrap'
+import Options from '../OrgManage/options.component'
+import Participants from '../OrgManage/participants.component'
+import Contact from '../OrgManage/contact.component'
+import { Container, Col, Row, Button, Collapse } from 'reactstrap'
+import { HashRouter, NavLink, Route } from 'react-router-dom'
 import '../../assets/scss/org_sign.scss'
+
+
 class OrgManage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             orgName: 'VIT Music Club',
-            isOpen : true
+            isOpen: true
         }
 
-        
+
     }
-    
+
     resize() {
         this.setState()
     }
-    
+
 
     render() {
         const toggle = () => {
@@ -25,27 +31,27 @@ class OrgManage extends Component {
                 isOpen: !this.state.isOpen
             })
         }
-        const placeholder_text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolorum fugit, sequi nisi minus commodi quam quae numquam repudiandae magnam. Dolore eum iure ducimus quibusdam neque. Quibusdam ex dolore sint!'
         return (
-            <div>
-                <Navbar1 />
+            <HashRouter>
+                <div>
+                    <Navbar1 />
 
-                <section className="section section-shaped">
-                    <div className="shape shape-style-1 shape-default">
+                    <section className="section section-shaped">
+                        <div className="shape shape-style-1 shape-default">
 
-                    </div>
-                    <Container className="py-md">
-
-
+                        </div>
+                        <Container className="py-md">
 
 
-                        <Row className="justify-content-between align-items-center">
-                            <Col className="mb-lg-auto" lg="4">
-                                <div style={{ margin: "auto", textAlign: "center" }}>
-                                    <span className="text-white" style={{ fontSize: "1.5rem" }}>
-                                        Hi <b>{this.state.orgName}</b>
-                                    </span>
-                                    {/* <img
+
+
+                            <Row className="justify-content-between align-items-center">
+                                <Col className="mb-lg-auto" lg="4">
+                                    <div style={{ margin: "auto", textAlign: "center" }}>
+                                        <span className="text-white" style={{ fontSize: "1.5rem" }}>
+                                            Hi <b>{this.state.orgName}</b>
+                                        </span>
+                                        {/* <img
                                         alt="..."
                                         className="img-fluid"
                                         style={{ paddingBottom: "1em", height: "10em", display: 'block' }}
@@ -54,30 +60,36 @@ class OrgManage extends Component {
 
                                     /> */}
 
-                                    <span className="manage">Management Console</span>
-                                    <div className='options-opener' onClick={toggle} >Options</div>
-                                    <Collapse isOpen={this.state.isOpen}>
-                                        <div className="options-holder">
+                                        <span className="manage">Management Console</span>
+                                        <div className='options-opener' onClick={toggle} >Options</div>
+                                        <Collapse isOpen={this.state.isOpen}>
+                                            <div className="options-holder">
 
 
-                                            <div className="options" tabIndex="0">Questions</div>
-                                            <div className="options" tabIndex="0">Participants</div>
-                                            <div className="options" tabIndex="0">Contact Us</div>
-                                        </div>
-                                    </Collapse>
-                                    
-                                </div>
-                            </Col>
-                            <Col className="mb-lg-auto" lg="8">
-                                <div className="placeholder">{placeholder_text}{placeholder_text}{placeholder_text}</div>
-                            </Col>
-                        </Row>
+                                                <NavLink to='/options'><div className="options" tabIndex="0">Questions</div></NavLink>
+                                                <NavLink to='/participants'><div className="options" tabIndex="0">Participants</div></NavLink>
+                                                <NavLink to='/contact'> <div className="options" tabIndex="0">Contact Us</div></NavLink>
+                                            </div>
+                                        </Collapse>
 
-                    </Container>
-                </section>
+                                    </div>
+                                </Col>
+                                <Col className="mb-lg-auto" lg="8">
+                                    <div className="placeholder">
+                                        <Route exact path='/options' component={Options} />
+                                        <Route path='/participants' component={Participants} />
+                                        <Route path='/contact' component={Contact} />
+                                    </div>
 
-                <Footer />
-            </div>
+                                </Col>
+                            </Row>
+
+                        </Container>
+                    </section>
+
+                    <Footer />
+                </div>
+            </HashRouter>
         )
     }
 }
---
+++ src/components/OrgManage/options.component.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Options extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state={}
+    }
+
+    render() {
+        return (
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga similique accusantium voluptas, laborum vel perferendis, illum qui blanditiis temporibus minus ex esse atque nisi? Laborum esse fugiat odit quaerat dolores.
+            </div>
+        )
+    }
+}
+
+export default Options;
