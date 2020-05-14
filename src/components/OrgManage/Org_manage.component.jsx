48bd237e004d83b1883e0e326e7b52dd28482693
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2020-05-14 16:22:48 +0530

    fixes to org page

--- src/components/OrgManage/Org_manage.component.jsx
+++ src/components/OrgManage/Org_manage.component.jsx
@@ -4,8 +4,8 @@ import Footer from '../footer.component';
 import Options from '../OrgManage/options.component'
 import Participants from '../OrgManage/participants.component'
 import Contact from '../OrgManage/contact.component'
-import { Container, Col, Row, Button, Collapse } from 'reactstrap'
-import { HashRouter, NavLink, Route } from 'react-router-dom'
+import { Container, Col, Row, Button, Collapse, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+import { HashRouter, NavLink, Route, Link } from 'react-router-dom'
 import '../../assets/scss/org_sign.scss'
 import QuestionHolder from './questions/taskForm';
 
@@ -34,28 +34,28 @@ class OrgManage extends Component {
         }
         return (
             <div>
-            <Navbar1 />
+                <Navbar1 />
                 <HashRouter>
-                
-                <div>
-                    
 
-                    <section className="section section-shaped">
-                        <div className="shape shape-style-1 shape-default">
-
-                        </div>
-                        <Container className="py-md">
+                    <div>
 
 
+                        <section className="section section-shaped">
+                            <div className="shape shape-style-1 shape-default">
+
+                            </div>
+                            <Container className="py-md">
 
 
-                            <Row className="justify-content-between align-items-center">
-                                <Col className="mb-lg-auto" lg="6">
-                                    <div style={{ margin: "auto", textAlign: "center" }}>
-                                        <span className="text-white" style={{ fontSize: "1.5rem" }}>
-                                            Hi <b>{this.state.orgName}</b>
-                                        </span>
-                                        {/* <img
+
+
+                                <Row className="justify-content-between align-items-center">
+                                    <Col className="mb-lg-auto" lg="6">
+                                        <div style={{ margin: "auto", textAlign: "center" }}>
+                                            <span className="text-white" style={{ fontSize: "1.5rem" }}>
+                                                Hi <b>{this.state.orgName}</b>
+                                            </span>
+                                            {/* <img
                                         alt="..."
                                         className="img-fluid"
                                         style={{ paddingBottom: "1em", height: "10em", display: 'block' }}
@@ -64,51 +64,65 @@ class OrgManage extends Component {
 
                                     /> */}
 
-                                        <span className="manage">Management Console</span>
-                                       </div>
+                                            <span className="manage">Management Console</span>
+                                        </div>
                                     </Col>
                                     <Col>
                                         <div className="center-tag">
-                                        <div className='options-opener' onClick={toggle} >Options</div>                                     
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            
-                                            <Collapse isOpen={this.state.isOpen}>                                                                               
 
 
-<div className="center-tag">
-                                            <div className="options-holder">
 
 
-                                                <NavLink exact to='/options'><div className="options" tabIndex="0">Questions</div></NavLink>
-                                                <NavLink exact to='/participants'><div className="options" tabIndex="0">Participants</div></NavLink>
-                                                <NavLink exact to='/contact'> <div className="options" tabIndex="0">Contact Us</div></NavLink>
+                                            <UncontrolledDropdown nav>
+                                                <DropdownToggle nav>
+                                                    <div className="options-opener">
+                                                        <span className="nav-link-inner--text">Options</span>
+                                                    </div>
+                                                </DropdownToggle>
+                                                <DropdownMenu>
+                                                    <DropdownItem to="/options" tag={Link}>
+                                                        <NavLink exact to='/options'><div className="options" tabIndex="0">Questions</div></NavLink>
+                                                    </DropdownItem>
+                                                    <DropdownItem to="/participants" tag={Link}>
+                                                        <NavLink exact to='/participants'><div className="options" tabIndex="0">Participants</div></NavLink>
+                                                    </DropdownItem>
+                                                    <DropdownItem to="/contact" tag={Link}>
+                                                        <NavLink exact to='/contact'> <div className="options" tabIndex="0">Contact Us</div></NavLink>
+                                                    </DropdownItem>
+
+                                                </DropdownMenu>
+                                            </UncontrolledDropdown>
+                                            {/* 
+                                            <Collapse isOpen={this.state.isOpen}>
+
+
+                                                <div className="center-tag">
+                                                    <div className="options-holder">
+
+
                                                     </div>
                                                 </div>
-                                        </Collapse>
+                                            </Collapse> */}
 
-                                    </div>
+                                        </div>
                                     </Col>
                                 </Row>
                                 <Row>
-                                <Col className="mb-lg-auto">
-                                    <div className="placeholder">
-                                        <Route exact path='/options' component={QuestionHolder} />
-                                        <Route path='/participants' component={Participants} />
-                                        <Route path='/contact' component={Contact} />
-                                    </div>
+                                    <Col className="mb-lg-auto">
+                                        <div className="placeholder">
+                                            <Route exact path='/options' component={QuestionHolder} />
+                                            <Route path='/participants' component={Participants} />
+                                            <Route path='/contact' component={Contact} />
+                                        </div>
 
-                                </Col>
-                            </Row>
+                                    </Col>
+                                </Row>
 
-                        </Container>
-                    </section>
+                            </Container>
+                        </section>
 
-                    <Footer />
-                </div>
+                        <Footer />
+                    </div>
                 </HashRouter>
             </div>
         )
