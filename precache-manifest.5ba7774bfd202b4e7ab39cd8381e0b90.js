a24e0546b7757dc3f67623905fb93ec4f5860393
Sanket Mohapatra <www.sanket1mohapatra@gmail.com>
2020-08-22 21:12:41 +0530

    Updates

---
+++ precache-manifest.5ba7774bfd202b4e7ab39cd8381e0b90.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddbcb49deeb2b473160b56530a1aafe",
+    "url": "/easyrecruit/index.html"
+  },
+  {
+    "revision": "0597506dcfd393fa0253",
+    "url": "/easyrecruit/static/css/main.d6a27ba4.chunk.css"
+  },
+  {
+    "revision": "d28008ac15f509862bd3",
+    "url": "/easyrecruit/static/js/2.3386b4ae.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/easyrecruit/static/js/2.3386b4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0597506dcfd393fa0253",
+    "url": "/easyrecruit/static/js/main.4406a8e6.chunk.js"
+  },
+  {
+    "revision": "be4debecfa068edebe95",
+    "url": "/easyrecruit/static/js/runtime-main.77c721fa.js"
+  },
+  {
+    "revision": "325e01e0164ca020b7fc1fd2e7adca2a",
+    "url": "/easyrecruit/static/media/15.325e01e0.png"
+  },
+  {
+    "revision": "671f7afd8afe7a8019c968a3f8e28ad3",
+    "url": "/easyrecruit/static/media/delivery-4.671f7afd.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/easyrecruit/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/easyrecruit/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/easyrecruit/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/easyrecruit/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/easyrecruit/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "734e292845c036579e8693153f471c6c",
+    "url": "/easyrecruit/static/media/global_error.734e2928.svg"
+  },
+  {
+    "revision": "defe0b5f0ad3fd7cf723531db1ac8a4f",
+    "url": "/easyrecruit/static/media/happy.defe0b5f.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/easyrecruit/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/easyrecruit/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/easyrecruit/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/easyrecruit/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/easyrecruit/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "141ed1fb218e38c4d8e57c37454a3fe8",
+    "url": "/easyrecruit/static/media/selection.141ed1fb.svg"
+  },
+  {
+    "revision": "56e652d2bf5fdef900aa4b68cdc8b784",
+    "url": "/easyrecruit/static/media/user_signup.56e652d2.png"
+  },
+  {
+    "revision": "d9d9b4620955fe62e3637a5ab7850d17",
+    "url": "/easyrecruit/static/media/white.d9d9b462.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8b261cd8da1d8b04356d45540b173c9.js"
+  "/easyrecruit/precache-manifest.5ba7774bfd202b4e7ab39cd8381e0b90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/easyrecruit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
