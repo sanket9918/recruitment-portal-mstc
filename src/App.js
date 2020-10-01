f3eb8e7f7d76fa568d1c5478efb98e5b73c5a76e
Sanket Mohapatra <www.sanket1mohapatra@gmail.com>
2020-10-01 21:38:26 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import './App.css';
-import Login from './components/hero-login.component';
-import Error from './components/error.component';
-import Error1 from './components/error1.component'
-import Navbar1 from './components/navbar.component';
-import Finish from './components/end_exam.component';
-import CandProfile from './components/user_overview.component';
-import OrgSignUp from './components/org_signup.component';
-import UserSignUp from './components/candidate_signup.component';
-import Exam from './components/quiz/exam.component';
-import OrgManage from './components/OrgManage/Org_manage.component';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import store from './store'
-import ProtectedRoute from './components/protectedRoute';
-
-
+import React from "react";
+import "./App.css";
+import Login from "./components/hero-login.component";
+import Error from "./components/error.component";
+import Error1 from "./components/error1.component";
+import Navbar1 from "./components/navbar.component";
+import Finish from "./components/end_exam.component";
+import CandProfile from "./components/user_overview.component";
+import OrgSignUp from "./components/org_signup.component";
+import UserSignUp from "./components/candidate_signup.component";
+import Exam from "./components/quiz/exam.component";
+import OrgManage from "./components/OrgManage/Org_manage.component";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import ProtectedRoute from "./components/protectedRoute";
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
           <div>
             <Navbar1 />
             <Switch>
-              <ProtectedRoute exact path='/overview' component={CandProfile} />
-              <ProtectedRoute path='/exam' exact component={Exam} />
-              <ProtectedRoute path='/orgmanage' exact component={OrgManage} />
-              <ProtectedRoute path='/finish' exact component={Finish} />
+              <ProtectedRoute exact path="/overview" component={CandProfile} />
+              <ProtectedRoute path="/exam" exact component={Exam} />
+              <ProtectedRoute path="/orgmanage" exact component={OrgManage} />
+              <ProtectedRoute path="/finish" exact component={Finish} />
 
-
-              <Route path='/' exact render={props => <Login {...props} />} />
-              <Route path='/error1' exact render={props => <Error1 {...props} />} />
-              <Route path='/error' exact render={props => <Error {...props} />} />
-              <Route path='/orgsignup' exact render={props => <OrgSignUp {...props} />} />
-              <Route path='/candsignup' exact render={props => <UserSignUp {...props} />} />
+              <Route path="/" exact render={(props) => <Login {...props} />} />
+              <Route
+                path="/error1"
+                exact
+                render={(props) => <Error1 {...props} />}
+              />
+              <Route
+                path="/error"
+                exact
+                render={(props) => <Error {...props} />}
+              />
+              <Route
+                path="/orgsignup"
+                exact
+                render={(props) => <OrgSignUp {...props} />}
+              />
+              <Route
+                path="/candsignup"
+                exact
+                render={(props) => <UserSignUp {...props} />}
+              />
               <Route component={Error} />
             </Switch>
           </div>
